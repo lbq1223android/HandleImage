acb4a46ba4db908ec724f91b92f2d6e77732adcf
haha <1904708437@qq.com>
2016-06-22 10:36:01 +0800

    加了个空格

--- app/src/main/java/com/oasis/handleimage/MainActivity.java
+++ app/src/main/java/com/oasis/handleimage/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); 
 
         Toast.makeText(this,"123",Toast.LENGTH_LONG).show();
 
