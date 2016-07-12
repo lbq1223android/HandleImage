4aea526c963ba6bcd53d3288007009a296d1b371
oasis-oliwen <lbq1223android@gmail.com>
2016-07-12 21:51:51 +0800

    滤镜调试完成  滤镜图片储存完成

--- app/src/main/java/com/oasis/handleimage/bean/FuctionBean.java
+++ app/src/main/java/com/oasis/handleimage/bean/FuctionBean.java
@@ -1,11 +1,18 @@
 package com.oasis.handleimage.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by liling on 2016/6/22.
  */
-public class FuctionBean {
+public class FuctionBean implements Parcelable {
     public String funcionName ;
     public Class className ;
+
+
+
+
 
     public  FuctionBean(String funcionName,Class className){
         this.funcionName = funcionName ;
@@ -27,4 +34,35 @@ public class FuctionBean {
     public void setClassName(Class className) {
         this.className = className;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.funcionName);
+        dest.writeSerializable(this.className);
+    }
+
+    public FuctionBean() {
+    }
+
+    protected FuctionBean(Parcel in) {
+        this.funcionName = in.readString();
+        this.className = (Class) in.readSerializable();
+    }
+
+    public static final Parcelable.Creator<FuctionBean> CREATOR = new Parcelable.Creator<FuctionBean>() {
+        @Override
+        public FuctionBean createFromParcel(Parcel source) {
+            return new FuctionBean(source);
+        }
+
+        @Override
+        public FuctionBean[] newArray(int size) {
+            return new FuctionBean[size];
+        }
+    };
 }
---
+++ app/src/main/java/com/oasis/handleimage/ui/Reverse.java
@@ -0,0 +1,73 @@
+package com.oasis.handleimage.ui;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.oasis.handleimage.BaseActivity;
+import com.oasis.handleimage.R;
+import com.oasis.handleimage.util.PhotoUtils;
+
+public class Reverse extends BaseActivity {
+
+    Button fanzhuanButton , zuoyoujingmin ,shangxiajingmian ;
+
+        ImageView imageView ;
+        Bitmap bb ;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.r);
+
+            fanzhuanButton = (Button) findViewById(R.id.button4);
+            zuoyoujingmin = (Button) findViewById(R.id.button5);
+            shangxiajingmian = (Button) findViewById(R.id.button8);
+
+        imageView = (ImageView) findViewById(R.id.imageView2);
+
+
+        bb = BitmapFactory.decodeResource(getResources(),R.mipmap.rt) ;
+        imageView.setImageBitmap(bb);
+
+        fanzhuanButton.setOnClickListener(new View.OnClickListener() {
+
+            Bitmap bit = bb;
+            float dee = 0f;
+            @Override
+            public void onClick(View v) {
+                bit = PhotoUtils.rotateImage(bit, 90);
+                dee +=90;
+                if(dee > 360)
+                {
+                    dee = 0;
+                }
+                //picture.setAnimation(rotateAnimation);
+
+                imageView.setImageBitmap(bit);
+            }
+        });
+
+        zuoyoujingmin.setOnClickListener(new View.OnClickListener() {
+            Bitmap bit = bb;
+            @Override
+            public void onClick(View v) {
+                bit = PhotoUtils.reverseImage(bit,-1,1) ;
+                imageView.setImageBitmap(bit);
+            }
+        });
+
+        shangxiajingmian.setOnClickListener(new View.OnClickListener() {
+            Bitmap bit = bb;
+            @Override
+            public void onClick(View v) {
+                bit = PhotoUtils.reverseImage(bit,1,-1) ;
+                imageView.setImageBitmap(bit);
+            }
+        });
+
+
+    }
+}
