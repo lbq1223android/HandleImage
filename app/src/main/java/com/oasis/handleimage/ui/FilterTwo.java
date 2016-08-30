32edef5335cdcc1be16690d363cb6ca8644594e7
haha <1904708437@qq.com>
2016-08-30 17:18:39 +0800

    Merge branch 'master' of https://github.com/lbq1223android/HandleImage
    Conflicts:
    	app/src/main/java/com/oasis/handleimage/MainActivity.java

--- app/src/main/java/com/oasis/handleimage/ui/FilterTwo.java
+++ app/src/main/java/com/oasis/handleimage/ui/FilterTwo.java
@@ -8,8 +8,10 @@ import android.widget.ImageView;
 
 import com.oasis.handleimage.BaseActivity;
 import com.oasis.handleimage.R;
+import com.zomato.photofilters.geometry.Point;
 import com.zomato.photofilters.imageprocessors.Filter;
 import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter;
+import com.zomato.photofilters.imageprocessors.subfilters.ToneCurveSubFilter;
 
 
 /**
@@ -31,19 +33,53 @@ public class FilterTwo extends BaseActivity {
 
         _imageView = (ImageView) findViewById(R.id.imageView2);
 
-        _bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.rt) ;
+        _bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.photo) ;
         _imageView.setImageBitmap(_bitmap);
 
         findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Filter filter = new Filter() ;
-                filter.addSubFilter(new SaturationSubfilter(1.5f));
+                Filter filter =  getNightWhisperFilter() ;
                 Bitmap outputImage = filter.processFilter(_bitmap.copy(Bitmap.Config.ARGB_8888,true)
                 );
                 _imageView.setImageBitmap(outputImage);
             }
         });
+
+
+
         
+    }
+
+    public  Filter getNightWhisperFilter() {
+        Point[] rgbKnots;
+        Point[] redKnots;
+        Point[] greenKnots;
+        Point[] blueKnots;
+
+        rgbKnots = new Point[3];
+        rgbKnots[0] = new Point(0, 0);
+        rgbKnots[1] = new Point(174, 109);
+        rgbKnots[2] = new Point(255, 255);
+
+        redKnots = new Point[4];
+        redKnots[0] = new Point(0, 0);
+        redKnots[1] = new Point(70, 114);
+        redKnots[2] = new Point(157, 145);
+        redKnots[3] = new Point(255, 255);
+
+        greenKnots = new Point[3];
+        greenKnots[0] = new Point(0, 0);
+        greenKnots[1] = new Point(109, 138);
+        greenKnots[2] = new Point(255, 255);
+
+        blueKnots = new Point[3];
+        blueKnots[0] = new Point(0, 0);
+        blueKnots[1] = new Point(113, 152);
+        blueKnots[2] = new Point(255, 255);
+
+        Filter filter = new Filter();
+        filter.addSubFilter(new ToneCurveSubFilter(rgbKnots, redKnots, greenKnots, blueKnots));
+        return filter;
     }
 }
